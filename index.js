5820601d47ced111c3916d789674f2d9e050e571
Joey Quadros <joey_qpb@hotmail.com>
2022-10-31 18:04:18 -0300

    move player on keydown

--- index.js
+++ index.js
@@ -29,6 +29,28 @@ image.src = "./img/JoeyKemon_Map.png";
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
 
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position;
+    this.image = image;
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+
+const background = new Sprite({
+  position: {
+    x: -550,
+    y: -650,
+  },
+  image: image,
+});
+
+let backgroundImageX = -550;
+let playerImageX = -550;
+
 // importando a img para um obj JS
 // estamaos criando uma imagem como ojeto para renderiza-la no nosso canvas
 
@@ -40,20 +62,20 @@ playerImage.src = "./img/playerDown.png";
 // RELACIONADO A IMG DENTRO DA TAG CANVAS
 // isso chama a image para a tela
 
-image.onload = () => {
-  c.drawImage(image, -550, -650);
-  c.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width / 4,
-    playerImage.height,
-    canvas.width / 2 - playerImage.width / 4 / 1.5,
-    canvas.height / 2 - playerImage.height / 2,
-    playerImage.width / 4,
-    playerImage.height
-  );
-};
+// image.onload = () => {
+//   c.drawImage(image, -550, -650);
+//   c.drawImage(
+//     playerImage,
+//     0,
+//     0,
+//     playerImage.width / 4,
+//     playerImage.height,
+//     canvas.width / 2 - playerImage.width / 4 / 1.5,
+//     canvas.height / 2 - playerImage.height / 2,
+//     playerImage.width / 4,
+//     playerImage.height
+//   );
+// };
 
 // nossa img é mto pesada, ent tivemos que achar um jeito para ela carregar na tela
 // quando a img carregar dentro do nosso objeto "image" vamos chamar nossa função
@@ -82,3 +104,115 @@ image.onload = () => {
 // para posiciona-lo no centro utilizamos a largura e a altura do canvas como base
 // largura e altura
 // são relacionados ao posicionamento da renderização da img na tela
+
+// -------------------------------------------------------------------------------
+
+// Move Player Through Map on Keydown
+
+// window.addEventListener("keydown", (e) => {
+//   console.log(e);
+// });
+
+// aqui estamos add um evento para cada "keydown" no obj global(window)
+// dessa forma - cm o console.log() conseguimos ver as propriedades de cada tecla pressionada
+// faremos um switch case para definir oq fazermos quando a tecla for pressionada
+
+// precisaremos criar uma animação para quando o nosso player for andar
+// faremos um window.requestanimationframe para parar e ordenar funcionara quando chamarmos ela
+// traremos as img para essa função para q ela possa fzr a animação frame a frame
+// alteraremos suas posições conforme o keydown escolhido
+// como ela vai alterar é interessante criarmos uma variável para ela no inicio do codigo
+// mais interessante é criar uma classe, pois teremos de declarar varias coisas do msm
+// criaremos as img em cima da classe
+// ao usarmos um obj no constructor ñ precisaremos lembrar da ordem dos argumentos
+// teremos metodos para cirar as img
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+// para testar qual tecla foi pressionada criaremos uma variavel de obj
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  background.draw();
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 1.5,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
+
+  if (keys.w.pressed && lastKey === `w`) background.position.y += 3;
+  else if (keys.a.pressed && lastKey === `a`) background.position.x += 3;
+  else if (keys.s.pressed && lastKey === `s`) background.position.y -= 3;
+  else if (keys.d.pressed && lastKey === `d`) background.position.x -= 3;
+}
+
+// nos ifs adicionaremos a movimentação conforme a tecla for pressionada
+
+animate();
+let lastKey = ``;
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case `w`:
+      keys.w.pressed = true;
+      lastKey = `w`;
+      break;
+
+    case `a`:
+      keys.a.pressed = true;
+      lastKey = `a`;
+      break;
+
+    case `s`:
+      keys.s.pressed = true;
+      lastKey = `s`;
+      break;
+
+    case `d`:
+      keys.d.pressed = true;
+      lastKey = `d`;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case `w`:
+      keys.w.pressed = false;
+      break;
+
+    case `a`:
+      keys.a.pressed = false;
+      break;
+
+    case `s`:
+      keys.s.pressed = false;
+      break;
+
+    case `d`:
+      keys.d.pressed = false;
+      break;
+  }
+});
+
+// vamos alterar o status do pressed na nossa chave 'keys'
+// vamos precisar de outro evento keyup para voltar a ser falso
