81265893ba21efd3f73015e51731990186c9cd8e
Joey Quadros <joey_qpb@hotmail.com>
2022-10-31 16:50:34 -0300

    player img creation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Map Creation
+
 const canvas = document.querySelector("canvas");
 canvas.width = 1024;
 canvas.height = 576;
@@ -24,6 +26,9 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = "./img/JoeyKemon_Map.png";
 
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
 // importando a img para um obj JS
 // estamaos criando uma imagem como ojeto para renderiza-la no nosso canvas
 
@@ -36,9 +41,44 @@ image.src = "./img/JoeyKemon_Map.png";
 // isso chama a image para a tela
 
 image.onload = () => {
-  c.drawImage(image, -600, -550);
+  c.drawImage(image, -550, -650);
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 1.5,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
 };
+
 // nossa img é mto pesada, ent tivemos que achar um jeito para ela carregar na tela
 // quando a img carregar dentro do nosso objeto "image" vamos chamar nossa função
 // para exibir a image na tela
 // ela foi exportada do tilde com o zoom desejado (400%)
+
+// -------------------------------------------------------------------------------
+
+// Player Creation
+
+// const playerImage = new Image();
+// playerImage.src = "./img/playerDown.png";
+
+// criamos um obj para essa img, mas usaremos ela em conjunto com a arrow function
+// ela será declarada antes de sua chamda
+// image -> para que ela seja chamada DPS do nosso mapa, para ficar acima dele
+
+// 4 primeiros argumentos:
+// 1-> a cordenada x na qual quermos começar a cortar a img
+// 2-> a cordenada y na qual quermos começar a cortar a img
+// 1-> a cordenada x na qual quermos finalizar a cortar a img
+// 2-> a cordenada y na qual quermos finalizar a cortar a img
+
+// 4 ultimos argumentos:
+// a ref de x e y vai ser em relação a img ja posicionada
+// para posiciona-lo no centro utilizamos a largura e a altura do canvas como base
+// largura e altura
+// são relacionados ao posicionamento da renderização da img na tela
